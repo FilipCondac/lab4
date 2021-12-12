741ad6681abd60a287f91a1d45973c3a0014defc
filipcondac <filipcondac@gmail.com>
2021-12-12 10:20:29 +0000

    fixed sql queries

--- routes/main.js
+++ routes/main.js
@@ -1,50 +1,23 @@
 // Route handler for forum web app
 
-module.exports = function(app, forumData) {
+module.exports = function (app, forumData) {
 
     // Handle our routes
 
     // Home page
-    app.get('/',function(req,res){
+    app.get('/', function (req, res) {
         res.render('index.ejs', forumData)
     });
 
     // About page
-    app.get('/about',function(req,res){
+    app.get('/about', function (req, res) {
         res.render('about.ejs', forumData);
     });
 
     // View Posts page
-    app.get('/viewposts',function(req,res){
+    app.get('/viewposts', function (req, res) {
         // Query to select all posts from the database
-        let sqlquery = `SELECT   post_id, p.post_date, t.topic_title, p.post_title, p.post_content, u.username
-                        FROM     posts p
-                        JOIN     topics t
-                        ON       t.topic_id = p.topic_id
-                        JOIN     users u
-                        ON       u.user_id = p.user_id
-                        ORDER BY post_date`;
-
-        // Run the query
-        db.query(sqlquery, (err, result) => {
-          if (err) {
-             res.redirect('./');
-          }
-
-          // Pass results to the EJS page and view it
-          let data = Object.assign({}, forumData, {posts:result});
-          console.log(data)
-          res.render('viewposts.ejs', data);
-        });
-    });
-
-    // List Users page
-    app.get('/users',function(req,res){
-        // Query to select all users
-        let sqlquery = `SELECT   username, firstname, surname, country
-                        FROM     users 
-                        ORDER BY username;`
-                 
+        let sqlquery = 'SELECT * FROM v_viewposts'
         // Run the query
         db.query(sqlquery, (err, result) => {
             if (err) {
@@ -52,18 +25,45 @@ module.exports = function(app, forumData) {
             }
 
             // Pass results to the EJS page and view it
-            let data = Object.assign({}, forumData, {users:result});
+            let data = Object.assign({}, forumData, {
+                posts: result
+            });
+            console.log(data)
+            res.render('viewposts.ejs', data);
+        });
+    });
+
+    // List Users page
+    app.get('/users', function (req, res) {
+        // Query to select all users
+        let sqlquery = `SELECT * FROM v_users`
+
+        // Run the query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+
+            // Pass results to the EJS page and view it
+            let data = Object.assign({}, forumData, {
+                users: result
+            });
             console.log(data)
             res.render('users.ejs', data);
-        });                        
+        });
     });
 
     // List Topics page
-    app.get('/topics',function(req,res){
+    app.get('/topics', function (req, res) {
         // Query to select all topics
-        let sqlquery = `SELECT   topic_id, topic_title, topic_description
-                        FROM     topics
-                        ORDER BY topic_title`
+        let sqlquery = `SELECT *
+        FROM   posts p
+        JOIN   topics t
+        ON     t.topic_id=p.topic_id
+        JOIN     users u
+        ON       u.user_id = p.user_id
+        ORDER BY post_date`
+
 
         // Run the query       
         db.query(sqlquery, (err, result) => {
@@ -72,31 +72,41 @@ module.exports = function(app, forumData) {
             }
 
             // Pass results to the EJS page and view it
-            let data = Object.assign({}, forumData, {topics:result});
+            let data = Object.assign({}, forumData, {
+                topics: result,
+    
+            });
             console.log(data)
             res.render('topics.ejs', data);
-        });    
+        });
     });
 
     // Add a New Post page
-    app.get('/addpost',function(req,res){
+    app.get('/addpost', function (req, res) {
         // Set the initial values for the form
-        let initialvalues = {username: '', topic: '', title: '', content: ''}
+        let initialvalues = {
+            username: '',
+            topic: '',
+            title: '',
+            content: ''
+        }
 
         // Pass the data to the EJS page and view it
-        return renderAddNewPost(res, initialvalues, "") 
+        return renderAddNewPost(res, initialvalues, "")
     });
 
     // Helper function to 
     function renderAddNewPost(res, initialvalues, errormessage) {
-        let data = Object.assign({}, forumData, initialvalues, {errormessage:errormessage});
+        let data = Object.assign({}, forumData, initialvalues, {
+            errormessage: errormessage
+        });
         console.log(data)
         res.render("addpost.ejs", data);
-        return 
+        return
     }
 
     // Add a New Post page form handler
-    app.post('/postadded', function (req,res) {
+    app.post('/postadded', function (req, res) {
         let user_id = -1
         let topic_id = -1
 
@@ -106,7 +116,7 @@ module.exports = function(app, forumData) {
             if (err) {
                 return console.error(err.message);
             }
-            if (result.length==0) {
+            if (result.length == 0) {
                 return renderAddNewPost(res, req.body, "Can't find that user")
             }
             user_id = result[0].user_id
@@ -118,7 +128,7 @@ module.exports = function(app, forumData) {
                 if (err) {
                     return console.error(err.message);
                 }
-                if (result.length==0) {
+                if (result.length == 0) {
                     return renderAddNewPost(res, req.body, "Can't find that topic")
                 }
                 topic_id = result[0].topic_id
@@ -130,8 +140,8 @@ module.exports = function(app, forumData) {
                     if (err) {
                         return console.error(err.message);
                     }
-                    if (result[0].countmembership==0) {
-                        return renderAddNewPost(res, req.body,  "User is not a member of that topic")
+                    if (result[0].countmembership == 0) {
+                        return renderAddNewPost(res, req.body, "User is not a member of that topic")
                     }
 
                     // Everything is in order so insert the post
@@ -139,19 +149,18 @@ module.exports = function(app, forumData) {
                                 VALUES (now(), ?, ?, ?, ?)`;
                     let newrecord = [req.body.title, req.body.content, user_id, topic_id];
                     db.query(sqlquery, newrecord, (err, result) => {
-                    if (err) {
-                        return console.error(err.message);
-                    }
-                    else
-                        res.send('You post has been added to forum');
-                    });    
-                });                   
+                        if (err) {
+                            return console.error(err.message);
+                        } else
+                            res.send('You post has been added to forum');
+                    });
+                });
             });
         });
     });
 
     // Search for Posts page
-    app.get('/search',function(req,res){
+    app.get('/search', function (req, res) {
         res.render("search.ejs", forumData);
     });
 
@@ -163,19 +172,48 @@ module.exports = function(app, forumData) {
                         FROM   posts p
                         JOIN   topics t
                         ON     t.topic_id=p.topic_id
+                        JOIN     users u
+                        ON       u.user_id = p.user_id
                         WHERE  post_title LIKE ? OR post_content LIKE ?
                         ORDER BY post_date`
 
 
-                        
+
         db.query(sqlquery, [term, term], (err, result) => {
             if (err) {
                 res.redirect('./');
             }
 
-            let data = Object.assign({}, forumData, {posts:result});
-         
-          res.render("viewposts.ejs", data);
-        });      
+            let data = Object.assign({}, forumData, {
+                posts: result
+            });
+
+            res.render("viewposts.ejs", data);
+        });
     });
+
+    app.get('/user-details', function (req, res) {
+        let term = '%' + req.query.username + '%'
+        let sqlquery = `SELECT *
+                        FROM   posts p
+                        JOIN  users u
+                        ON u.user_id = p.user_id
+                        WHERE u.username = 'term'
+                        ORDER BY post_date`
+
+
+
+        db.query(sqlquery, [term, term], (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+
+            let data = Object.assign({}, forumData, {
+                posts: result
+            });
+
+            res.render("viewposts.ejs", data);
+        });
+    });
+
 }
